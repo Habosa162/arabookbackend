998f6d587bcd220d06382eece6bcbfee4b2ca335
Habosa162 <habosa2000@hotmail.com>
2022-04-24 02:15:31 +0200

    try for cpanel

--- src/Model/databaseCon/conDb.js
+++ src/Model/databaseCon/conDb.js
@@ -1,25 +1,4 @@
-// const connection = async()=>{
-//     const mysql = require("mysql2");
-//     const con  =  await mysql.createConnection({
-//         host :"127.0.0.1",
-//         user:"root",
-//         password:"root",
-//         database:"openbook"
-//     })
-
-//     con.connect((err)=>{
-//         if(err){throw err}else{
-//             console.log("___________________THE CONNECTION WITH DATABASE WORKS____________________");
-//         }
-//     })
-
-
-//     module.exports = con ;   
-// }
-// connection()
-
-
-    const mysql = require("mysql2");
+const mysql = require("mysql2");
 
 
     const con  = mysql.createConnection({
